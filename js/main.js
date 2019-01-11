077f87c0168846a4804225c02548b60ceb751cd8
Sergio Mínguez Gala <sergiomg@MacBook-Pro-de-Sergio.local>
2019-01-11 13:58:25 +0100

    mobile5

--- js/main.js
+++ js/main.js
@@ -50,13 +50,14 @@ function ScrollHandler(pageId) {
       scrolled.scrollTop = position;
       runAnimation = requestAnimationFrame(animate);
       stopAnimationIfRequired(position);
-      console.log("position=" + scrolled.scrollTop + "(" + percentage + ")");
+      //console.log("position=" + scrolled.scrollTop + "(" + percentage + ")");
     };
     // Loop the animation function
     runAnimation = requestAnimationFrame(animate);
   }
 
   window.addEventListener("wheel", function(event) {
+    console.log('eent')
     viewStart = scrolled.scrollTop;
     if (!pageJump) {
       var pageHeight = page.scrollHeight;
@@ -74,6 +75,7 @@ function ScrollHandler(pageId) {
       var stopJumpUp = pageEndPart > pageStopPortion;
 
       var scrollingForward = event.deltaY > 0;
+      console.log(scrollingForward)
       if (
         (scrollingForward && canJumpDown && !stopJumpDown) ||
         (!scrollingForward && canJumpUp && !stopJumpUp)
@@ -86,7 +88,83 @@ function ScrollHandler(pageId) {
       event.preventDefault();
     }
   });
+
+  /* MOUSE EVENTS */
+  document.addEventListener('touchstart', handleTouchStart, false);
+  document.addEventListener('touchmove', handleTouchMove, false);
+
+  var xDown = null;
+  var yDown = null;
+
+  function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+  }
+
+  function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+  };
+
+  function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */
+        } else {
+            /* right swipe */
+        }
+    } else {
+      viewStart = scrolled.scrollTop;
+      if (!pageJump) {
+        var pageHeight = page.scrollHeight;
+        var pageStopPortion = pageHeight / 2;
+        var viewHeight = window.innerHeight;
+
+        var viewEnd = viewStart + viewHeight;
+        var pageStartPart = viewEnd - pageStart;
+        var pageEndPart = pageStart + pageHeight - viewStart;
+
+        var canJumpDown = pageStartPart >= 0;
+        var stopJumpDown = pageStartPart > pageStopPortion;
+
+        var canJumpUp = pageEndPart >= 0;
+        var stopJumpUp = pageEndPart > pageStopPortion;
+
+        var scrollingForward = yDiff > 0 ? true : false;
+        console.log(scrollingForward)
+        if (
+          (scrollingForward && canJumpDown && !stopJumpDown) ||
+          (!scrollingForward && canJumpUp && !stopJumpUp)
+        ) {
+          evt.preventDefault();
+          scrollToPage();
+        }
+        false; //
+      } else {
+        evt.preventDefault();
+      }
+
+
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+  };
+
 }
+
+
 new ScrollHandler("home");
 new ScrollHandler("project1");
 new ScrollHandler("project2");
